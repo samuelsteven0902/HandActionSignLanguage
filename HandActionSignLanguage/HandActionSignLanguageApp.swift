884c81cdc1673f168ea86f3f4ac209483a2d38e8
samuelsteven0902 <samuelsteven@student.uns.ac.id>
2024-05-31 15:38:30 +0700

    Initial Commit

---
+++ HandActionSignLanguage/HandActionSignLanguageApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandActionSignLanguageApp.swift
+//  HandActionSignLanguage
+//
+//  Created by samuel on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct HandActionSignLanguageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
